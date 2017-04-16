34a70168285a3da7d1e2828b38491d817c9715d5
YogurtYoung <antoshkabest@ua.fm>
2017-04-16 05:03:05 +0300

    add the join table books_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416015157) do
+ActiveRecord::Schema.define(version: 20170416020000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170416015157) do
     t.datetime "updated_at",       null: false
   end
 
+  create_table "books_categories", id: false, force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "category_id"
+    t.index ["book_id"], name: "index_books_categories_on_book_id", using: :btree
+    t.index ["category_id"], name: "index_books_categories_on_category_id", using: :btree
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
@@ -66,4 +73,6 @@ ActiveRecord::Schema.define(version: 20170416015157) do
 
   add_foreign_key "authors_books", "authors"
   add_foreign_key "authors_books", "books"
+  add_foreign_key "books_categories", "books"
+  add_foreign_key "books_categories", "categories"
 end
