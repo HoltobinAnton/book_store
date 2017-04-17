41f0fe07e97db01c06c096c7465b899f7792cb90
YogurtYoung <antoshkabest@ua.fm>
2017-04-17 07:00:09 +0300

    add the check_secret_method to coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,2 +1,6 @@
 class Coupon < ApplicationRecord
+  
+  def check_secret_code?(str_code)
+    self.secret_code.eql?(str_code)
+  end
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Check the secret code' do
+    it 'should pass code validation' do
+      coupon = create(:coupon)
+      expect(coupon.check_secret_code?('Q12345')).to be true
+    end
+    it 'should not pass the code validation' do
+      coupon = create(:coupon)
+      expect(coupon.check_secret_code?('test_secret_code')).to be false
+    end
+  end
+
+  
 end
