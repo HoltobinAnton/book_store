a76d9d5cdfa46ab1c47f853f5da9e2a07bc12b9b
YogurtYoung <antoshkabest@ua.fm>
2017-05-19 01:41:47 +0300

    change reviews rating type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518022150) do
+ActiveRecord::Schema.define(version: 20170518223901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 20170518022150) do
   create_table "reviews", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "rating"
+    t.float    "rating"
     t.integer  "book_id"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
