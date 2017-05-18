508355cb3413bf9e1099288a8a777e228ddce69e
YogurtYoung <antoshkabest@ua.fm>
2017-05-18 10:32:16 +0300

    edit validations in reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,7 @@
 class Review < ApplicationRecord
   validates :title, :description, :rating, presence: true
-  validates_format_of :title, with: /\A[a-z\d]*\Z/i
   validates_length_of :title, maximum: 80
-
+  validates_length_of :description, maximum: 500
   belongs_to :book
   belongs_to :user
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,26 +11,8 @@ RSpec.describe Review, type: :model do
       it { is_expected.to validate_presence_of(attr) }
     end
   end
-
-  context 'the title should be validated' do
-    it 'should be validated on  a-z, A-Z, 0-9,' do
-      should allow_value('Test12345').for(:title)
-      should allow_value('name').for(:title)
-    end
-
+  context 'Check for the number of characters' do
     it { should validate_length_of(:title).is_at_most(80) }
+    it { should validate_length_of(:description).is_at_most(500) }
   end
-
-  context 'the tite should not be validated on regulars' do
-    ['Test 14',
-     '!@fawfw',
-     ' 1f',
-     'test-test',
-     '{fawfwfw}',
-     ' !#$%&*+-/=?^_`{|}~.'].each do |str|
-      it 'the tite should not be validated on regulars' do
-        should_not allow_value(str).for(:title)
-      end
-    end
-  end 
 end
