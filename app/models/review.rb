b6bfa212b346a722d4a90fbf7fbcab5284d4165c
YogurtYoung <antoshkabest@ua.fm>
2017-05-19 01:38:00 +0300

    add validations on the rating for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   validates :title, :description, :rating, presence: true
   validates_length_of :title, maximum: 80
   validates_length_of :description, maximum: 500
+  validates_numericality_of :rating, greater_than: 0, less_than_or_equal_to: 5
   belongs_to :book
   belongs_to :user
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,8 +11,19 @@ RSpec.describe Review, type: :model do
       it { is_expected.to validate_presence_of(attr) }
     end
   end
+
   context 'Check for the number of characters' do
     it { should validate_length_of(:title).is_at_most(80) }
     it { should validate_length_of(:description).is_at_most(500) }
+  end
+
+  context 'the rating should greater than 0 and less than or equal 5' do
+    it 'tha rating should greater tnan 0' do
+      should validate_numericality_of(:rating).is_greater_than(0)
+    end
+
+    it 'the rating should less than or equal 5' do
+      should validate_numericality_of(:rating).is_less_than_or_equal_to(5)
+    end
   end
 end
